4c641e170a74c484be0cff58abe5a8a387e3501e
Ulring <anasssoulimani@gmail.com>
2021-04-15 09:32:48 +0000

    Day002

--- handmade/code/win32_handmade.cpp
+++ handmade/code/win32_handmade.cpp
@@ -1,22 +1,129 @@
 /*###################################################################
 
 #File Name: win32_handmade.cpp
-#Author: Anass Soulimani 
+#Author: Anass Soulimani
 #Email: anasssoulimani@gmail.com
 #Creation Date: Sun 18 Oct 2020 05:25:46 AM +01
-#Last Modified: Tue 20 Oct 2020 02:09:28 AM +01
+#Last Modified: Thu 15 Apr 2021 09:04:15 AM +00
 
 ###################################################################*/
 
 #include <windows.h>
 
-int CALLBACK 
-WinMain(
-		HINSTANCE hInstance,
-		HINSTANCE hPrevInstance,
-		LPSTR lpCmdLine,
-		int nCmdShow)
+LRESULT CALLBACK
+MainWindowCallback(HWND Window,
+				   UINT Message,
+				   WPARAM WParam,
+				   LPARAM LParam)
 {
-	MessageBoxA(0, "This is Handmade Hero.", "Handmade Hero", MB_OK|MB_ICONINFORMATION);
+	LRESULT Result = 0;
+
+	switch(Message)
+	{
+		case WM_SIZE:
+		{
+			OutputDebugStringA("WM_SIZE\n");
+		} break;
+		case WM_DESTROY:
+		{
+			OutputDebugStringA("WM_DESTROY\n");
+		} break;
+		case WM_CLOSE:
+		{
+			OutputDebugStringA("WM_CLOSE\n");
+		} break;
+		case WM_ACTIVATEAPP:
+		{
+			OutputDebugStringA("WM_ACTIVATEAPP\n");
+		} break;
+		case WM_PAINT:
+		{
+			PAINTSTRUCT Paint;
+			HDC DeviceContext = BeginPaint(Window, &Paint);
+			int X = Paint.rcPaint.left;
+			int Y = Paint.rcPaint.top;
+			int Width = Paint.rcPaint.right - Paint.rcPaint.left;
+			int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+			static DWORD Operation = WHITENESS;
+ 			PatBlt(DeviceContext, X, Y, Width, Height, Operation);
+			if(Operation == WHITENESS)
+			{
+				Operation = BLACKNESS;
+			}
+			else
+			{
+				Operation = WHITENESS;
+			}
+			EndPaint(Window, &Paint);
+		} break;
+
+		default:
+		{
+			//OutputDebugStringA("default\n");
+			Result = DefWindowProc(Window, Message, WParam, LParam);
+		} break;
+	}
+
+	return(Result);
+}
+
+int CALLBACK
+WinMain(
+		HINSTANCE Instance,
+		HINSTANCE PrevInstance,
+		LPSTR CommandLine,
+		int ShowCode)
+{
+	WNDCLASS WindowClass = {};
+
+	//WindowClass.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW;
+	WindowClass.lpfnWndProc = MainWindowCallback;
+	WindowClass.hInstance = Instance;
+	//WindowClass.hIcon;
+	WindowClass.lpszClassName = "HandmadeHeroWindowClass";
+
+	if(RegisterClassA(&WindowClass))
+	{
+		HWND WindowHandle =
+			CreateWindowExA(
+				0,
+				WindowClass.lpszClassName,
+				"Handmade Hero",
+				WS_OVERLAPPEDWINDOW|WS_VISIBLE,
+				CW_USEDEFAULT,
+				CW_USEDEFAULT,
+				CW_USEDEFAULT,
+				CW_USEDEFAULT,
+				0,
+				0,
+				Instance,
+				0);
+		//if(WindowHandle != NULL)
+		if(WindowHandle)
+		{
+			for(;;){
+				MSG Message;
+				BOOL MessageResult = GetMessageA(&Message, 0, 0, 0);
+				if(MessageResult > 0)
+				{
+					TranslateMessage(&Message);
+					DispatchMessageA(&Message);
+				}
+				else
+				{
+					break;
+				}
+			}
+		}
+		else
+		{
+			//TODO(anass): Logging
+		}
+	}
+	else
+	{
+		//TODO(anass): Logging
+	}
+
 	return(0);
 }
--- handmade/misc/shell.bat
+++ handmade/misc/shell.bat
@@ -1,6 +1,6 @@
 @echo off
 
-REM To add this shortcut in cmd:
+REM To add this shortcut in cmd to run at startup:
 REM %windir%\system32\cmd.exe /k C:\Users\Anass\Desktop\work\handmade\misc\shell.bat 
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall" x64
